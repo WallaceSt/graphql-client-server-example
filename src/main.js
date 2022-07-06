e928d1ac07d7a233295c197ad2d09f3d4243cd35
Wallace <wallacycleyton@gmail.com>
2022-07-05 23:59:30 -0300

    Criando servidor simples

--- src/main.js
+++ src/main.js
@@ -1,1 +1,23 @@
-console.log("Hello World!");
+import { createServer } from "http";
+
+const server = createServer((request, response) => {
+    switch (request.url) {
+        case "/status": {
+            response.writeHead(200);
+            response.write("Okay");
+            response.end();
+            break;
+        }
+        default: {
+            response.writeHead(404, "Service not found");
+            response.end();
+        }
+    }
+});
+
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;
+const HOSTNAME = process.env.HOSTNAME || '127.0.0.1';
+
+server.listen(PORT, '127.0.0.1', () => {
+    console.log(`Server is listening at http://${HOSTNAME}:${PORT}.`);
+});
