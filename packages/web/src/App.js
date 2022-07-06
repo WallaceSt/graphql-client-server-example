866efa003780d0d0d69d7102c0d66d712b5ec487
Wallace <wallacycleyton@gmail.com>
2022-07-06 10:55:23 -0300

    Separando front do back e configurando react no front

---
+++ packages/web/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Signin from './pages/Signin';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={ <Home /> } />
+      <Route path="/sign-in" element={ <Signin /> } />
+    </Routes>
+  );
+}
+
+export default App;
