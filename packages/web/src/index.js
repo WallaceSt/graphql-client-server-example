a7c8b9ed8d30faa02334364ff616f0c17380fac8
Wallace <wallacycleyton@gmail.com>
2022-07-08 17:06:06 -0300

    Configurando Apollo Link e permitindo paginacao simples em Home

--- packages/web/src/index.js
+++ packages/web/src/index.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { ApolloProvider } from "@apollo/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import client from "./plugins/apollo/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ApolloProvider client={ client }>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ApolloProvider>
   </React.StrictMode>
 );
 
--- packages/web/src/plugins/apollo/client.js
+++ packages/web/src/plugins/apollo/client.js
@@ -1,14 +1,30 @@
-import { ApolloClient } from 'apollo-client';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-
-import link from "./link";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloLink,
+  Observable,
+} from "@apollo/client";
 
 const cache = new InMemoryCache();
+const loggerLink = new ApolloLink((operation, forward) => {
+  new Observable((observer) => {
+    const subscription = forward(operation).subscribe({
+      next: (result) => {
+        console.log("Log", result);
+        observer.next(result);
+      },
+      error: observer.error.bind(observer),
+      complete: observer.complete.bind(observer),
+    });
+    return () => subscription.unsubscribe();
+  });
+});
 
 const client = new ApolloClient({
-    link,
-    cache,
-    connectToDevTools: true,
-})
+    uri: "http://localhost:8000/graphql",
+    loggerLink,
+  cache,
+  connectToDevTools: true,
+});
 
 export default client;
